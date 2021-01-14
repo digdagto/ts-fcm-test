2e22ca1f830c833e78be8e59ed19e3e51cb66b1c
taeseokchoi <digdagto@gmail.com>
2021-01-14 20:32:03 +0900

    service worker changed for to work in background

--- messaging/firebase-messaging-sw.js
+++ messaging/firebase-messaging-sw.js
@@ -1,11 +1,12 @@
 // Import and configure the Firebase SDK
 // These scripts are made available when the app is served or deployed on Firebase Hosting
 // If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
-importScripts('/__/firebase/8.2.2/firebase-app.js');
-importScripts('/__/firebase/8.2.2/firebase-messaging.js');
-importScripts('/__/firebase/init.js');
+importScripts('https://www.gstatic.com/firebasejs/8.2.2/firebase-analytics.js');
+importScripts('https://www.gstatic.com/firebasejs/8.2.2/firebase-messaging.js');
+
 
 const messaging = firebase.messaging();
+
 
 /**
  * Here is is the code snippet to initialize Firebase Messaging in the Service
