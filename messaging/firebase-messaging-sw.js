d7afc9d01c5bd79d4ff414be88e0d39959ffce91
taeseokchoi <digdagto@gmail.com>
2021-01-14 20:39:41 +0900

    importScripts to import

--- messaging/firebase-messaging-sw.js
+++ messaging/firebase-messaging-sw.js
@@ -1,9 +1,9 @@
 // Import and configure the Firebase SDK
 // These scripts are made available when the app is served or deployed on Firebase Hosting
 // If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
-importScripts('https://www.gstatic.com/firebasejs/8.2.2/firebase-analytics.js');
-importScripts('https://www.gstatic.com/firebasejs/8.2.2/firebase-messaging.js');
-importScripts('https://www.gstatic.com/firebasejs/8.2.2/firebase-app.js');
+import ('https://www.gstatic.com/firebasejs/8.2.2/firebase-analytics.js');
+import ('https://www.gstatic.com/firebasejs/8.2.2/firebase-messaging.js');
+import ('https://www.gstatic.com/firebasejs/8.2.2/firebase-app.js');
 
 const messaging = firebase.messaging();
 
